bc2cc32deba52cd6cdc843c30046cdfde05221ad
DIOLUIS <diogoparazita@gmail.com>
2021-05-11 12:14:22 -0300

    Importado pacotes do git

--- Parte1/packages/main.go
+++ Parte1/packages/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 	"module/auxiliar"
+
+	//pacote importado do git
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+	//chamando um pacote externo
+	erro := checkmail.ValidateFormat("diogo@123.com")
+	fmt.Println(erro)
 }
